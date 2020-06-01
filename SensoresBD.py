840a68cfd1167a2e53c8291a504c1e5dfea97137
JorgeGBravo <jdgbravo@gmail.com>
2020-06-01 18:05:41 +0100

    extracción datos json -Sensores Parking-

--- SensoresBD.py
+++ SensoresBD.py
@@ -4,32 +4,73 @@ import urllib.parse
 import urllib.error
 import ssl
 import random
+import sqlite3
 
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-
 url = 'http://datos.gob.es/apidata/catalog/dataset' + '/title/{}'.format('sensores')
 print(url)
 
 urlimp = urllib.request.urlopen(url)
 
+comm = sqlite3.connect('sensores.sqlite')
+cur = comm.cursor()
 
-print('URL     :', urlimp.geturl())
+# Sensores Parking Superficie - Zona Azul- Santander
+
+cur.execute('''CREATE TABLE IF NOT EXISTS pkazulsantder
+                    (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                    type TEXT UNIQUE, identifier INTERGER UNIQUE, status INTERGER,
+                    modified TIMESTAMP, latitude FLOAT, longitude FLOAT,
+                    uri TEXT)''')
+
+# Sensores Medioambientales
+
+cur.execute('''CREATE TABLE IF NOT EXISTS smedioambientalsantder
+                    (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    type TEXT UNIQUE, identifier INTERGER UNIQUE, noise FLOAT, temperature FLOAT,
+                    light FLOAT, battery FLOAT, modified TIMESTAMP, latitude FLOAT,
+                    longitude FLOAT, uri TEXT)''')
+
+# Sensores de Riego
+
+cur.execute('''CREATE TABLE IF NOT EXISTS irrigacion
+                    (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    type TEXT UNIQUE, identifier INTERGER UNIQUE, soilmoisturetension FLOAT,
+                    temperature FLOAT, winddirection FLOAT, rainfall FLOAT, radiationpar FLOAT,
+                    solarradiation FLOAT, windspeed FLOAT, groudtemperature FLOAT, atmosphericpreassure FLOAT,
+                    relativehumidity FLOAT, battery FLOAT, modified TIMESTAMP, latitude FLOAT, longitude FLOAT,
+                    uri TEXT) ''')
+
+# Sensores Moviles Medioambientales - Santander -
+
+cur.execute('''CREATE TABLE IF NOT EXISTS movambientalsantder
+                    (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    type TEXT UNIQUE, identifier INTERGER UNIQUE, particles FLOAT, NO2 FLOAT, temperature FLOAT,
+                    altitude FLOAT, speed FLOAT, CO FLOAT, odometer FLOAT, course FLOAT, ozone FLOAT,
+                    modified TIMESTAMP, latitude FLOAT, longitude FLOAT,
+                    uri TEXT)''')
+
+# Sensores Parking - Villanueva de la Serena - Badajoz -
+
+cur.execute('''CREATE TABLE IF NOT EXISTS parkingvillser
+                    (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    nombre_parking TEXT UNIQUE, num_plaza INTERGER UNIQUE, codigo_estado INTERGER, tipo_plaza TEXT,
+                    latitude FLOAT, longitude FLOAT)''')
+
+print('URL     :' , urlimp.geturl())
 
 headers = urlimp.info()
-print('DATE    :', headers['date'])
+print('DATE    :' , headers['date'])
 print('HEADERS :')
 print('---------')
 print(headers)
 
 data = urlimp.read().decode()
 print('LENGTH  :', len(data))
-#print('DATA    :')
-print('---------')
-#print(data)
 
 try:
     js = json.loads(str(data))
@@ -37,37 +78,79 @@ except:
     js = None
 
 #print(json.dumps(js, indent=4))
-lista = list()
 
 for formatos in js['result']['items']:
-    #print('items:')
-    #print(json.dumps(formatos, indent=4))
     distri = js['result']['items']
 
 for popa in distri:
     abo = popa['distribution']
 
     for pope in abo:
-        #print('pope', json.dumps(pope, indent=4))
         titulo = pope['title']
         accessU = pope['accessURL']
         format = pope['format']
         value = format['value']
 
-        if value == 'application/vnd.geo+json' not in lista:
-            print(titulo)
-            print(accessU)
-            urlgj = urllib.request.urlopen(accessU).read().decode()
-            print('****************************************************')
-            print(json.dumps(urlgj , indent=4))
-            print('****************************************************')
+        if value == 'application/vnd.geo+json':
 
-        if value == 'application/json' not in lista:
-            print(titulo)
-            print(accessU)
-            urlj = urllib.request.urlopen(accessU).read().decode()
-            print('****************************************************')
-            print(json.dumps(urlj , indent=4))
-            print('****************************************************')
+            if titulo == 'Sensores del parking':
+                print(titulo)
+                print(accessU)
+                urlsp = urllib.request.urlopen(accessU)
 
-        # print(lista)
+                datasp = urlsp.read().decode()
+
+                print(json.dumps(datasp , indent=4))
+
+                try:
+                    js = json.loads(str(datasp))
+                except:
+                    js = None
+
+                feature = js['features']
+                for i in feature:
+                    geo = i['geometry']
+                    prop = i['properties']
+
+                    for g in geo:
+                        coor = geo['coordinates']
+                        lat = geo['coordinates'][1]
+                        long = geo['coordinates'][0]
+
+                    for p in prop:
+                        num_plaza = prop['num_plaza']
+                        cod_estado = prop['cod_estado']
+                        estado = prop['estado']
+                        tipo_plaza = prop['tipo_plaza']
+                        print(tipo_plaza)
+
+            if titulo == 'Sensores de EESystem':
+                print(titulo)
+                print(accessU)
+                urlsee = urllib.request.urlopen(accessU).read().decode()
+                print(json.dumps(urlsee , indent=4))
+
+        if value == 'application/json':
+            if titulo == 'Sensores ambientales':
+                print(titulo)
+                print(accessU)
+                urlsa = urllib.request.urlopen(accessU).read().decode()
+                print(json.dumps(urlsa , indent=4))
+
+            if titulo == 'Sensores Parking de Superficie':
+                print(titulo)
+                print(accessU)
+                urlsps = urllib.request.urlopen(accessU).read().decode()
+                print(json.dumps(urlsps , indent=4))
+
+            if titulo == 'Sensores de riego':
+                print(titulo)
+                print(accessU)
+                urlsr = urllib.request.urlopen(accessU).read().decode()
+                print(json.dumps(urlsr , indent=4))
+
+            if titulo == 'Sensores móviles':
+                print(titulo)
+                print(accessU)
+                urlsm = urllib.request.urlopen(accessU).read().decode()
+                print(json.dumps(urlsm , indent=4))
